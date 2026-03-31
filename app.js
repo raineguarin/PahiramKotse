640989354593f4090632033635eb9ebfd0eda742
chingketchup <ching.ketchup@gmail.com>
2026-03-31 14:30:55 +0800

    possible fix?

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 // use node app.js in cli to check if connection works
 // after use http://localhost:3000/ on a browser to check that the homepage renders
 
-/*const dns = require('node:dns');
-dns.setServers(['8.8.8.8', '1.1.1.1']);*/
+const dns = require('node:dns');
+dns.setServers(['8.8.8.8', '1.1.1.1']); 
 
 const express = require('express');
 const mongoose = require('mongoose');
@@ -12,10 +12,8 @@ const user = require('./model/user');
 const app = express();
 const exphbs = require('express-handlebars');
 
-require('dotenv').config();
-
 // DATABASE CONNECTION 
-mongoose.connect(process.env.URI)
+mongoose.connect('mongodb+srv://PahiramKoAdmin:Group4Apdev@pahiramkotse.g6rovco.mongodb.net/pahiramKotseDB?retryWrites=true&w=majority')
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...', err));
 
@@ -45,7 +43,7 @@ app.engine('hbs', exphbs.engine({
 }));
 
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'view')); 
+app.set('views', path.join(process.cwd(), 'view'));
 
 // MIDDLEWARE
 app.use(express.static('public'));
@@ -86,7 +84,4 @@ app.use('/', reservationRoutes);
 app.use('/', customerRoutes);
 
 // SERVER START
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+module.exports = app;
